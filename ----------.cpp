8e98b272e3ecf479fdbf20a394b4af6592392cd4
user <user@DESKTOP-AQJHK0A>
2024-12-20 17:43:13 +0100

    Добавьте файлы проекта.

---
+++ ----------.cpp
@@ -0,0 +1,58 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[] = { 1, 2, 3, 4, 5 };
+    int b[5];
+    int* p1 = a, * p2 = b;
+
+    for (int i = 0; i < 5; i++) {
+        *(p2 + i) = *(p1 + i);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout << b[i] << " ";
+    }
+
+
+
+
+
+
+
+
+    int a[] = { 1, 2, 3, 4, 5 };
+    int* p = a;
+
+    for (int i = 4; i >= 0; i--) {
+        cout << *(p + i) << " ";
+    }
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+    int a[] = { 1, 2, 3, 4, 5 };
+    int b[5];
+    int* p1 = a, * p2 = b;
+
+    for (int i = 0; i < 5; i++) {
+        *(p2 + i) = *(p1 + 4 - i);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout << b[i] << " ";
+    }
+
+    return 0;
+    return 0;
+}
+
